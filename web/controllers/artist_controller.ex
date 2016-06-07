615190e1de83881dc08e3e2e79f8f11ba73e430c
Nate Piche <nfpiche@mtu.edu>
2016-06-06 20:59:43 -0400

    adds filtering by artist name

--- web/controllers/artist_controller.ex
+++ web/controllers/artist_controller.ex
@@ -3,13 +3,25 @@ defmodule PitchforkApi.ArtistController do
   alias PitchforkApi.Repo
   alias PitchforkApi.Artist
 
-  def index(conn, _params) do
-    artists = Repo.all(Artist)
+  whitelist_params = ~w(name)
+  use Inquisitor, with: PitchforkApi.Artist, whitelist: whitelist_params
+
+  def index(conn, params) do
+    artists =
+      build_artist_query(params)
+      |> Repo.all()
+
     render(conn, artists: artists)
   end
 
   def show(conn, %{"id" => id}) do
     artist = Repo.get(Artist, id)
     render conn, artist: artist
+  end
+
+  defp build_artist_query(query, [{"name", name}|tail]) do
+    query
+    |> Ecto.Query.where([a], ilike(a.name, ^"#{name}%"))
+    |> build_artist_query(tail)
   end
 end
