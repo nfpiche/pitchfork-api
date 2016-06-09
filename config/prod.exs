0963c9464bbf39f3f503f7f65c44386e11c760bb
Nate Piche <nfpiche@mtu.edu>
2016-06-09 19:52:50 -0400

    heroku

--- config/prod.exs
+++ config/prod.exs
@@ -3,7 +3,7 @@ use Mix.Config
 config :pitchfork_api, PitchforkApi.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [host: "https://afternoon-waters-20741.herokuapp.com/", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
 config :logger, level: :info
 
