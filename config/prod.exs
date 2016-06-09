78b8433547aa8a1fa5d8063ef09cfda407e8bf12
Nate Piche <nfpiche@mtu.edu>
2016-06-09 19:36:38 -0400

    remove static manifest

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,6 @@ use Mix.Config
 config :pitchfork_api, PitchforkApi.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production
 config :logger, level: :info
