66e587e5796d7b80ee2431af8d28669e6313337e
Nate Piche <nfpiche@mtu.edu>
2016-06-09 20:10:39 -0400

    heroku

--- config/prod.exs
+++ config/prod.exs
@@ -2,9 +2,14 @@ use Mix.Config
 
 config :pitchfork_api, PitchforkApi.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "https://afternoon-waters-20741.herokuapp.com/", port: 443],
+  url: [scheme: "https", host: "shrouded-badlands-35511.herokuapp.com", port: 443],
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
   force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
-config :logger, level: :info
+config :pitchfork_api, PitchforkApi.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL")
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 
-import_config "prod.secret.exs"
+config :logger, level: :info
